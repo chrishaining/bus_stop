a7899b8ba173fa18e816560d7e26267eaf5da4ae
Chris Haining <chrishaining@yahoo.co.uk>
2019-08-06 19:56:22 +0100

    part b completed

--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -1,11 +1,13 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../bus')
+require_relative('../person')
 
 class BusTest < MiniTest::Test
 
   def setup
-    @bus = Bus.new("22", "Ocean Terminal")
+    passengers = []
+    @bus = Bus.new("22", "Ocean Terminal", passengers)
   end
 
   def test_route_number
@@ -16,8 +18,30 @@ class BusTest < MiniTest::Test
     assert_equal("Ocean Terminal", @bus.destination())
   end
 
-def test_make_driving_sound
-  assert_equal("Brum brum", @bus.make_driving_sound())
-end
+  def test_make_driving_sound
+    assert_equal("Brum brum", @bus.make_driving_sound())
+  end
+
+  def test_count_passengers
+    assert_equal(0, @bus.count_passengers())
+  end
+
+  def test_count_passengers__pick_up
+    passenger1 = Person.new("Chris", 43)
+    @bus.pick_up(passenger1)
+    assert_equal(1, @bus.count_passengers())
+  end
+
+  def test_count_passengers__drop_off
+    passenger1 = Person.new("Chris", 43)
+    @bus.drop_off(passenger1)
+    assert_equal(0, @bus.count_passengers())
+  end
+
+  def test_emptying_bus
+    passenger1 = Person.new("Chris", 43)
+    @bus.emptying_bus()
+    assert_equal(0, @bus.count_passengers())
+  end
 
 end
